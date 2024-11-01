9e23dcbb8b9ab1dce0e8b32a799a7acd630f14e4
NQTin <tinnguyen.080104@gmail.com>
2024-11-01 10:05:44 -0400

    completed ci build

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -135,6 +135,12 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
+    # Test cant get account
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
     # List account
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
